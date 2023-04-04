c3221a1a77a7097b0a9d45d2437444c2dbe16112
R1CH4RD25 <richard.sullivan2002@gmail.com>
2023-04-04 15:53:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,70 +1,88 @@
-from datetime import datetime
+# To turn this app,
+# 0. Make a copy (Fork) of this repl with your repl account
+# 1. install required packages in Shell:  pip install streamlit yfinance mysql-connector-python plotly
+# 2. type  streamlit run streamlit_app.py  within the Shell
 
+# import packages needed
+from datetime import date, datetime
 import streamlit as st
-from vega_datasets import data
+import yfinance as yf
+import plotly.express as px
+import mysql.connector
+import pandas as pd
 
-from utils import chart, db
+# print title
+st.title("CIDM6351 Streamlit Demo")
+# print plain text
+st.write(
+    "**Streamlit**: The fastest way to build and share data apps in Python")
+st.write(
+    "Here is the cheat sheet of Streamlit. It summarized features of Streamlit: https://docs.streamlit.io/library/cheatsheet#display-progress-and-status"
+)
 
-COMMENT_TEMPLATE_MD = """{} - {}
-> {}"""
+# print text formatted by HTML
+st.markdown(
+    "<h1 style='text-align:center; color:red;'> Sample Stock Price App. </h1>",
+    unsafe_allow_html=True)
 
+st.write(
+    'We will use the code learned from previous lecture to build an online application for tracing stock price'
+)
 
-def space(num_lines=1):
-    """Adds empty lines to the Streamlit app."""
-    for _ in range(num_lines):
-        st.write("")
+# print text in markdown
+st.markdown("## **Check Stock Information**")
 
+# a list of stock names
+stock_names = ['MSFT', 'AAPL', 'AMZN', 'GOOGL']
+# select a stock to check
+target_stock = st.selectbox('Select a stock to check', options=stock_names)
 
-st.set_page_config(layout="centered", page_icon="💬", page_title="Commenting app")
+st.markdown("## **Check Stock Price History**")
 
-# Data visualisation part
+# start date of the stock infomation, default is the first day of year 2021
+start_date = st.date_input('Start Date', datetime(2021, 1, 1))
+# end date of the stock infomation, default is date of today
+end_date = st.date_input("End Date")
 
-st.title("💬 Commenting app")
+# get today date
+today = date.today()
+if st.button('Submit'):
+    # check valid date
+    if start_date > today or end_date > today:
+        st.write("## **Please select a valid date period.**")
+    else:
+        # download the stock data based on stock name, start/end date
+        data = yf.download(target_stock, start_date, end_date)
+        # show a progress bar
+        with st.spinner(text='In progress'):
 
-source = data.stocks()
-all_symbols = source.symbol.unique()
-symbols = st.multiselect("Choose stocks to visualize", all_symbols, all_symbols[:3])
+            fig = px.line(data,
+                          x=data.index,
+                          y=['Open', 'High', 'Low', 'Close'],
+                          title=target_stock + " Stock Price",
+                          labels={
+                              "value": "Stock Price ($)",
+                              "variable": "Price Type"
+                          })
+            st.write(fig)
+            st.success('Done')
 
-space(1)
+st.markdown(
+    "<h1 style='text-align:center; color:red;'> Show Data From Database </h1>",
+    unsafe_allow_html=True)
 
-source = source[source.symbol.isin(symbols)]
-chart = chart.get_chart(source)
-st.altair_chart(chart, use_container_width=True)
+# Define MySQL Connection String
+cnx = mysql.connector.connect(
+    user='cidm6351',  # username of database
+    password='dataETL@WT',  # password of database
+    host='20.172.0.16',  # ip address of the database
+    database='cidm6351',  # target database 
+    port=8080  # the default port of MySQL is 3306.     
+)
 
-space(2)
+if st.button('See All Patients Records'):
 
-# Comments part
-
-conn = db.connect()
-comments = db.collect(conn)
-
-with st.expander("💬 Open comments"):
-
-    # Show comments
-
-    st.write("**Comments:**")
-
-    for index, entry in enumerate(comments.itertuples()):
-        st.markdown(COMMENT_TEMPLATE_MD.format(entry.name, entry.date, entry.comment))
-
-        is_last = index == len(comments) - 1
-        is_new = "just_posted" in st.session_state and is_last
-        if is_new:
-            st.success("☝️ Your comment was successfully posted.")
-
-    space(2)
-
-    # Insert comment
-
-    st.write("**Add your own comment:**")
-    form = st.form("comment")
-    name = form.text_input("Name")
-    comment = form.text_area("Comment")
-    submit = form.form_submit_button("Add comment")
-
-    if submit:
-        date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        db.insert(conn, [[name, comment, date]])
-        if "just_posted" not in st.session_state:
-            st.session_state["just_posted"] = True
-        st.experimental_rerun()
+    query = "SELECT * FROM patients"
+    # use pandas to query records
+    df = pd.read_sql(query, cnx)
+    st.dataframe(df)
